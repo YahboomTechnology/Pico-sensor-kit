de09d5912f056b08748e9f7e79d91058bfe5e43f
Yahboom Technology <35594203+YahboomTechnology@users.noreply.github.com>
2024-09-14 18:19:41 +0800

    Add files via upload

--- 5.Library file/ir.py
+++ 5.Library file/ir.py
@@ -1,62 +1,59 @@
 import time
 from machine import Pin
 
-
-class ir():
-    def __init__(self, _Pin):
-        self.Pin = _Pin
+class IRReceiver:
+    def __init__(self, pin):
+        self.Pin = pin
         self.ir_repeat_cnt = 0
         self.irdata = 0xfe
-            
+
     def Getir(self):
         if self.Pin.value() == 0:
-            
             self.ir_repeat_cnt = 0
             count = 0
             while self.Pin.value() == 0:
                 count += 1
-                time.sleep(0.00003)
+                time.sleep_us(30)  
 
             count = 0
             while self.Pin.value() == 1 and count < 160:
                 count += 1
-                time.sleep(0.00003)
+                time.sleep_us(30)
             print(" ")
             idx = 0
             cnt = 0
-            data = [0,0,0,0]
-            for i in range(0,32):
+            data = [0, 0, 0, 0]
+            for i in range(0, 32):
                 count = 0
                 while self.Pin.value() == 0 and count < 30:
                     count += 1
-                    time.sleep(0.00003)
+                    time.sleep_us(30)
 
                 count = 0
                 while self.Pin.value() == 1 and count < 80:
                     count += 1
-                    time.sleep(0.00003)
+                    time.sleep_us(30)
 
                 if count > 35:
-                    data[idx] |= 1<<cnt
+                    data[idx] |= 1 << cnt
                 if cnt == 7:
                     cnt = 0
                     idx += 1
                 else:
                     cnt += 1
-            
-            if data[0]+data[1] == 0xFF and data[2]+data[3] == 0xFF:
+
+            if data[0] + data[1] == 0xFF and data[2] + data[3] == 0xFF:
                 self.irdata = data[2]
+                print("Decoded data:", data)
         else:
-            if self.ir_repeat_cnt > 110: 
+            if self.ir_repeat_cnt > 110:
                 self.ir_repeat_cnt = 0
                 self.irdata = 0xfe
             else:
-                time.sleep(0.001)
+                time.sleep_us(1000)
                 self.ir_repeat_cnt += 1
-        if self.irdata != None:
-            if self.irdata != 254:
-                result = self.irdata
-                # self.irdata = 254
-                return result
-        
-
+        if self.irdata != 254:
+            result = self.irdata
+            self.irdata = 254
+            return result
+        return None
